262ffa0598f449d1fde72d8c0ef420d738e1d611
Pawan Pandey <pandeypawan686800@gmail.com>
2026-04-19 00:27:40 +0530

    fixed cart routing and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link ,NavLink } from "react-router-dom";
 import {MapPin } from "lucide-react";
 import { FaCaretDown } from "react-icons/fa";
+import { IoCart } from "react-icons/io5";
+import Products from './../pages/Products';
+import About from './../pages/About';
+import Contact from './../pages/Contact';
+
 
 
 
@@ -12,9 +17,9 @@ const Navbar = () => {
     const location = false;
 
     return (
-        <div className='bg-white shadow-2xl py-3 h-auto'>
+        <div className='bg-white flex justify-around  shadow-2xl py-3 h-auto'>
 
-            <div className='max-w-4xl mx-auto flex justify-center items-center'>
+            <div className='max-w-4xl  flex  items-center'>
                 {/* LOGO IS HERE */}
                 <div className='flex gap-7'>
                     <Link to={'/'}><h className='font-bold'><span className='text-green-400'>Shop</span>Store</h></Link>
@@ -34,9 +39,25 @@ const Navbar = () => {
                 </div>
             </div>
 {/* 
-   NAVBAR          */}
+ NAVBAR       
+    */}
 
-   <nav></nav>
+   <nav className='flex gap-7 items-center '>
+    <ul className='flex gap-7 items-center text-sm  font-semibold'>
+     <NavLink to={"/"} className={({isActive})=>`${isActive ? "text-green-500 border-b-2 border-green-400 transition-all":"text-black"} cursor-pointer`}> <li>Home</li> </NavLink>
+     <NavLink to={'/Products'}  className={({isActive})=>`${isActive ? "text-green-500 border-b-2 border-green-400 transition-all":"text-black"} cursor-pointer`}> <li>Products</li> </NavLink>
+     <NavLink to={'/About'}  className={({isActive})=>`${isActive ? "text-green-500 border-b-2 border-green-400 transition-all":"text-black"} cursor-pointer`}> <li>About</li > </NavLink>
+     <NavLink to={'/Contact'}  className={({isActive})=>`${isActive ? "text-green-500 border-b-2 border-green-400 transition-all":"text-black"} cursor-pointer`}> <li>Contact</li> </NavLink>
+   </ul>
+
+   {/* Cart is HERE */}
+
+<Link to={'/Cart'} className='flex relative'> 
+ <IoCart className=' h-7 w-7' />
+ <span className='bg-green-500 text-white rounded-[50%] px-1.5 text-sm absolute -top-2 -right-3'>0</span>
+</Link>
+  
+   </nav>
 
 
 
